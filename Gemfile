217e12d6883c2513c796e54fd8fb278ae970b18f
Mike Foley <mike@signaturelabsinc.com>
2013-06-10 18:18:15 -0500

    add initial rails-api framework

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'rails-api'
+gem 'sqlite3'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+NewsDemo::Application.config.secret_token = '6e437fd0fdc06856e14932e5f4a415acefb665be3715fd59147c5d9624f201b973dc2efd2d112105692b596ab3d4991112a13efd811be28a2c9a40407d994685'
